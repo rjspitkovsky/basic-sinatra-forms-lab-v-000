9af1270b03eaf6cee4d41e529476ab6c78ba7cc6
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-22 19:34:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,12 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-  get '/newteam' do 
+  get '/newteam' do
     erb :newteam
   end
 
-  post '/team' do 
-    erb :team 
+  post '/team' do
+    @name = params[:name] 
+    @coach = params[:coach]
+    @point_guard = params[:pg]
+    @shooting_guard = params[:sg]
+    @small_forward = params[:sf]
+    @power_forward = params[:pf]
+    @center = params[:c]
+
+    
+    erb :team
   end 
 
 end
